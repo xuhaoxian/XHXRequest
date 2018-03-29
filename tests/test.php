eca3222d8a6873f63f0ba9a2034dd7d8f2a7aa85
xuhaoxian <312263441@qq.com>
2018-03-29 14:50:05 +0800

    提交

--- tests/test.php
+++ tests/test.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $query = array();
-$params = array("account"=>"13606910469", "password"=>"123123");
+$params = array("account"=>"13606910469", "password"=>"123456");
 $header = array();
 
 \XHXRequest\XHXRequest::getInstance()->request(
